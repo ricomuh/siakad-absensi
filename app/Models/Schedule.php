88ada28e06933aa62b56ec3918f294f4c127820b
ricomuh <rico.smartlolo@gmail.com>
2024-07-12 11:05:45 +0700

    feat: Add fillable attributes and relationships to Schedule and SubjectSession models

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,4 +8,67 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'class_room_id',
+        'subject_id',
+        'day',
+        'start_time',
+        'end_time',
+    ];
+
+    /**
+     * Get the class room that owns the schedule.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function classRoom()
+    {
+        return $this->belongsTo(ClassRoom::class);
+    }
+
+    /**
+     * Get the subject that owns the schedule.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    /**
+     * Get the schedule's day.
+     *
+     * @return string
+     */
+    public function getDayAttribute(): string
+    {
+        $days = [
+            0 => 'Sunday',
+            1 => 'Monday',
+            2 => 'Tuesday',
+            3 => 'Wednesday',
+            4 => 'Thursday',
+            5 => 'Friday',
+            6 => 'Saturday',
+        ];
+
+        return $days[$this->attributes['day']];
+    }
+
+    /**
+     * Get the schedule's sessions.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function sessions(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(SubjectSession::class);
+    }
 }
--- app/Models/SubjectSession.php
+++ app/Models/SubjectSession.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class SubjectSession extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'schedule_id',
+        'uuid',
+    ];
+
+    /**
+     * Get the schedule that owns the subject session.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function schedule()
+    {
+        return $this->belongsTo(Schedule::class);
+    }
 }
