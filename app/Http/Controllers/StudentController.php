144ff0a6f4a8f0b2a87b58cd9e42a13eef4594a9
ricomuh <rico.smartlolo@gmail.com>
2024-07-21 16:50:16 +0700

    Refactor StudentController for better code organization and readability

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\RoleEnum;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = User::students()->get();
+
+        return view('students.index', compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+        ]);
+
+        User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => bcrypt('password'),
+            'role_id' => RoleEnum::STUDENT,
+        ]);
+
+        return redirect()->route('students.index')->with('success', 'Student created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        $user->load([
+            'classRooms' => ['classRoom', 'subject'],
+        ]);
+
+        return view('students.show', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        return view('students.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->reset_password ? bcrypt('password') : $user->password,
+        ]);
+
+        return redirect()->route('students.index')->with('success', 'Student updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->classRooms()->delete();
+
+        $user->delete();
+
+        return redirect()->route('students.index')->with('success', 'Student deleted successfully.');
+    }
+}
