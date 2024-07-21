eb7be75fcfada34733239bfc3451b1d589339d47
ricomuh <rico.smartlolo@gmail.com>
2024-07-21 08:03:09 +0700

    feat: Add middleware for admin, teacher, and student roles
    This commit adds middleware classes for the admin, teacher, and student roles. The AdminMiddleware, TeacherMiddleware, and StudentMiddleware classes are responsible for handling incoming requests and checking if the authenticated user has the corresponding role. If the user does not have the required role, a 403 Forbidden response is returned. This addition enhances the application's security and access control by enforcing role-based authorization.

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\RoleEnum;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        abort_if(auth()->user()->role_id !== RoleEnum::ADMIN, 403);
+
+        return $next($request);
+    }
+}
