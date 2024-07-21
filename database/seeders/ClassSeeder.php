60c668145dc9bbefbf379b3b89dc978453e0b119
ricomuh <rico.smartlolo@gmail.com>
2024-07-21 07:26:40 +0700

    feat: Add subjects, class rooms, and student relationships to ClassSeeder
    This commit adds the necessary code to the ClassSeeder to create subjects, class rooms, and establish relationships with students. It generates subjects for each grade and assigns them to the corresponding class rooms. Additionally, it attaches random students to each class room. This update enhances the functionality of the ClassSeeder and allows for better management of subjects and students in the system.

--- database/seeders/ClassSeeder.php
+++ database/seeders/ClassSeeder.php
@@ -18,11 +18,36 @@ class ClassSeeder extends Seeder
     {
         $grades = collect([7, 8, 9]);
 
-        $classRooms = $grades->map(function ($grade) use ($teachers) {
+        $subjects = collect([
+            'Mathematics',
+            'Physics',
+            'Chemistry',
+            'Biology',
+            'History',
+        ]);
+
+        $createdSubjects = collect();
+
+        $classRooms = $grades->map(function ($grade) use ($teachers, $createdSubjects) {
+            $createdSubjects->push(
+                \App\Models\Subject::factory(5)->create([
+                    'grade' => $grade,
+                ])
+            );
+
             return \App\Models\ClassRoom::factory(3)->create([
                 'grade' => $grade,
                 'teacher_id' => $teachers->random()->id,
             ]);
+        });
+
+        $classRooms->each(function ($classRoom) use ($admins, $students) {
+            $classRoom->each(function ($class) use ($admins, $students) {
+                $class->subjects()->attach($class->grade, $class->grade + 1, $class->grade + 2);
+
+                $class->students()->attach($students->random(30)->pluck('id'));
+                // $class->students()->attach($admins->random(2)->pluck('id'));
+            });
         });
     }
 }
