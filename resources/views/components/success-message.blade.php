5df9251fd8333ab5613a7498d8f33cafbc3d8fbf
ricomuh <rico.smartlolo@gmail.com>
2024-07-22 15:27:22 +0700

    Refactor success-message.blade.php to use 'success' session variable
    This commit updates the success-message.blade.php file to use the 'success' session variable instead of 'message' for better clarity and consistency. It modifies the conditional statement to check for the 'success' session variable and displays the success message accordingly. This change improves the readability and maintainability of the codebase.

--- resources/views/components/success-message.blade.php
+++ resources/views/components/success-message.blade.php
@@ -1,10 +1,12 @@
-@if (session()->has('message'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    {{ session('message') }}
-    <!-- close button -->
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <!-- close icon -->
-        <span aria-hidden="true">&times;</span>
-    </button>
+@if (session()->has('success'))
+<div class="container">
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <!-- close button -->
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <!-- close icon -->
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 </div>
 @endif
