5ef472df59e49ef379e6a4d816b94dbdede3739c
ricomuh <rico.smartlolo@gmail.com>
2024-09-30 14:56:10 +0700

    Refactor subject show page to add DataTables buttons plugins for exporting and printing data

--- resources/views/teacher/subjects/show.blade.php
+++ resources/views/teacher/subjects/show.blade.php
@@ -173,12 +173,21 @@
 
     @push('scripts')
     <!-- DataTables  & Plugins -->
+    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    {{-- support datatables buttons plugins --}}
+    <script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+
 
     <script>
         $(function() {
@@ -190,6 +199,8 @@
                 "info": true,
                 "autoWidth": false,
                 "responsive": true,
+                "dom": "Bfrtip",
+                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
             });
 
             $('#presensi').DataTable({
@@ -200,6 +211,8 @@
                 "info": true,
                 "autoWidth": false,
                 "responsive": true,
+                "dom": "Bfrtip",
+                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
             });
         });
     </script>
